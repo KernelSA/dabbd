e73510a524d4aa21067cbb5e700aca18d1fb9ec6
Maxim.Panchenko <maks.pain@gmail.com>
2018-12-04 23:10:26 +0200

    WIP: handle deserialization exceptions

--- event-listener/src/main/java/ua/kernel/dabbd/eventlistener/config/ReceiverConfig.java
+++ event-listener/src/main/java/ua/kernel/dabbd/eventlistener/config/ReceiverConfig.java
@@ -12,6 +12,7 @@ import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer2;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import ua.kernel.dabbd.commons.model.TrackerEvent;
 
@@ -31,8 +32,15 @@ public class ReceiverConfig {
     @Bean
     public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, TrackerEvent>> trackerEventKafkaListenerContainerFactory() {
         Map<String, Object> props = getCommonConsumerProperties();
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+//        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+//        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer2.class);
+        props.put(ErrorHandlingDeserializer2.KEY_DESERIALIZER_CLASS, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer2.class);
+        props.put(ErrorHandlingDeserializer2.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
+//        props.put(JsonDeserializer.KEY_DEFAULT_TYPE, "com.example.MyKey");
+//        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, "com.example.MyValue");
+//        props.put(JsonDeserializer.TRUSTED_PACKAGES, "com.example");
 
         ConsumerFactory<String, TrackerEvent> consumerFactory = new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), new JsonDeserializer<>(TrackerEvent.class));
 
@@ -40,7 +48,9 @@ public class ReceiverConfig {
         factory.setConsumerFactory(consumerFactory);
         factory.setErrorHandler((thrownException, data) -> {
             log.error("Failed to read message: '" + data + "'", thrownException);
+
         });
+
         return factory;
     }
 
