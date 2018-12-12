a41fc4b139698826a18c49848366fbac9216f153
Maxim.Panchenko <maks.pain@gmail.com>
2018-12-13 01:03:53 +0200

    Load DB test 2

--- dabbd-db-load-test/src/main/java/ua/kernel/dabbd/loadtest/DbLoadTestApp.java
+++ dabbd-db-load-test/src/main/java/ua/kernel/dabbd/loadtest/DbLoadTestApp.java
@@ -12,6 +12,7 @@ import ua.kernel.dabbd.loadtest.db.EventsTestRepository;
 import javax.annotation.PostConstruct;
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.UUID;
 
 @Slf4j
@@ -23,31 +24,58 @@ public class DbLoadTestApp {
     @Autowired
     private EventsTestRepository eventsRepository;
 
-    private static String[] args;
+    private static String[] ARGS;
 
     public static void main(String[] args) {
+        if (args != null) {
+            Arrays.stream(args).forEach(s -> {
+
+                System.out.println("args=" + s);
+                log.info("arg={}", s);
+            });
+        }
+        ARGS = args;
+
         SpringApplication app = new SpringApplication(DbLoadTestApp.class);
         app.run(args);
     }
 
     @PostConstruct
     public void test() {
-        Integer numberToGenerate = 3_000_000;
-        if (args != null && args[0] != null) {
-            numberToGenerate = Integer.valueOf(args[0]);
+        Integer numberToGenerate = 10_000;
+        Integer threads = 3;
+        if (ARGS != null && ARGS.length > 0) {
+            numberToGenerate = Integer.valueOf(ARGS[0]);
+            if (ARGS.length > 2) {
+                threads = Integer.valueOf(ARGS[1]);
+            }
+
         }
-        log.info("Going to insert {} events in DB");
+
+        log.info("Going to insert {} events in {} threads to DB", numberToGenerate, threads);
         LocalDateTime start = LocalDateTime.now();
-        for (int i = 0; i < numberToGenerate; i++) {
-            eventsRepository.save(EventsTestEntity.builder()
-                    .trackerId(UUID.randomUUID().toString())
-                    .eventDt(LocalDateTime.now())
-                    .speed(1)
-                    .fuel(1)
-                    .power(1)
-                    .latitude(34.12)
-                    .longitude(12.34)
-                    .build());
+
+        int number = numberToGenerate;
+
+        for (int i = 0; i < threads; i++) {
+            int threadNum = i;
+            new Thread(() -> {
+                log.info("Thread {} started", threadNum);
+                for (int j = 0; j < number; j++) {
+                    eventsRepository.save(EventsTestEntity.builder()
+                            .trackerId(UUID.randomUUID().toString())
+                            .eventDt(LocalDateTime.now())
+                            .speed(1)
+                            .fuel(1)
+                            .power(1)
+                            .latitude(34.12)
+                            .longitude(12.34)
+                            .build());
+                }
+
+                log.info("Thread {} completed", threadNum);
+            }).start();
+
         }
         LocalDateTime end = LocalDateTime.now();
         log.info("{} events inserted in {}", Duration.between(start, end).toString());
