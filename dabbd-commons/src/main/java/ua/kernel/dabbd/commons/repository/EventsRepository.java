9e4b86824b3a6a7782410223f05b2fbfc812821d
Maxim.Panchenko <maks.pain@gmail.com>
2018-12-15 14:58:05 +0200

    Limit history for today

--- dabbd-commons/src/main/java/ua/kernel/dabbd/commons/repository/EventsRepository.java
+++ dabbd-commons/src/main/java/ua/kernel/dabbd/commons/repository/EventsRepository.java
@@ -4,11 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ua.kernel.dabbd.commons.entity.EventsEntity;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface EventsRepository extends JpaRepository<EventsEntity, Long> {
 
     List<EventsEntity> findAllByTrackerId(String trackerId);
+    List<EventsEntity> findByTrackerIdAndEveAndEventDtGreaterThan(String trackerId, LocalDateTime date);
 
 }
